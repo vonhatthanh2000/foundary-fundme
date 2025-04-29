96a7c4d7eba2994f7678db1f7af10376f625b9fc
thanhvodev <nthanhatee@gmail.com>
2025-04-29 17:41:37 +0700

    test and deploy script

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -15,4 +15,8 @@ contract FundMeTest is Test {
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
