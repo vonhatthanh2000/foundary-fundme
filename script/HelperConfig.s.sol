27ce94c8b69d9af1e5dfef433c3aefe34dca0dfb
thanhvo <nthanhatee@gmail.com>
2025-05-05 10:06:31 +0700

    helper with mock config

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getAnvilLocalConfig();
+        }
+    }
+
+    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilLocalConfig() public pure returns (NetworkConfig memory) {}
+}
