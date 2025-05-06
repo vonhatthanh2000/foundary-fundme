53df43e51f465cee2eefffb152b98334040536d1
thanhvo <nthanhatee@gmail.com>
2025-05-06 10:08:56 +0700

    add fake user and deal with initial balance

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,9 +3,13 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -26,5 +30,24 @@ contract HelperConfig is Script {
         return sepoliaConfig;
     }
 
-    function getAnvilLocalConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilLocalConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy the mocks
+        // 2. return the mock contract
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,9 +9,14 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant INITIAL_BALANCE = 10 ether;
+
     function setUp() external {
         DeployFundMe deployedFundMe = new DeployFundMe();
         fundMe = deployedFundMe.run();
+        vm.deal(USER, INITIAL_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -25,5 +30,19 @@ contract FundMeTest is Test {
     function testPriceFeedVersion() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundfailedWithoutEnoughETH() public {
+        vm.expectRevert(); // The next line should revert
+        fundMe.fund();
+    }
+
+    function testFundUpdatedFundedDataStructure() public {
+        vm.prank(USER); // the next tx will be sent by USER
+
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
